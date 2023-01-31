8942a1f0b257457cd03236f5d208b8890cbe4c9a
Gbamele Alexandre <alexdelor2022@gmail.com>
2023-01-31 09:25:21 +0100

    update Exercice Xp Gold

--- src/Circle.java
+++ src/Circle.java
@@ -1,27 +1,40 @@
-
-import java.math.*;
-
 public class Circle {
-    static double rayon1 = 2.0;
-    double rayon2 = 12;
-    double rayon3 = 24;
-    Circle circle1 = new Circle();
-    Circle circle2 = new Circle();
-    Circle circle3 = new Circle();
+    public double rayon = 1;
+    public static String messageAv = "Pour rayon egal a ";
+    public static String messageAp = " l'air du cercle vaut : ";
 
     /**
      * @param args
      *             Methode permettant de calculer l'air du cercle
      */
 
-    public static double CircleArea(double r) {
-        return Math.PI * (r * r);
+    public  double CircleArea() {
+        return Math.PI * (this.rayon * this.rayon);
     }
 
     public static void main(String[] args) {
+        Circle circle1 = new Circle(2.0);
+        Circle circle2 = new Circle(12);
+        Circle circle3 = new Circle(24);
 
-        System.out.println("L'air du premier cercle est : " + Air1);
+        System.out.println("L'air du premier cercle est : ");
+
+        double areaCircle1 = circle1.CircleArea();
+        double areaCircle2 = circle2.CircleArea();
+        double areaCircle3 = circle3.CircleArea();
+
+        System.out.println(messageAv + circle1.rayon + messageAp + areaCircle1 );
+        System.out.println(messageAv + circle2.rayon + messageAp + areaCircle2 );
+        System.out.println(messageAv + circle3.rayon + messageAp + areaCircle3  + ", ");
+        
 
     }
 
+    public Circle(double rayon) {
+        this.rayon = rayon;
+    }
+
+    public Circle(){
+       
+    }
 }
